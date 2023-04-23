315a6fa52f2a4301359604947d720e135caef64d
franky3020 <210238hwsh@gmail.com>
2023-04-23 21:43:11 +0800

    測試 物理引擎如何操作

--- src/app/page/test-matter-js-page/test-matter-js-page.component.ts
+++ src/app/page/test-matter-js-page/test-matter-js-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
-import { Engine, Runner, Render, World, Constraint, MouseConstraint, Bodies} from 'matter-js'
+import {Bodies, Engine, Render, Runner, World} from 'matter-js'
 import * as p5 from 'p5';
 
 @Component({
@@ -9,80 +9,61 @@ import * as p5 from 'p5';
 })
 export class TestMatterJsPageComponent {
 
-
-  @ViewChild('matter_canvas') matter_canvas: ElementRef<HTMLCanvasElement> | undefined;
   @ViewChild('p5_div') p5_div: ElementRef<HTMLDivElement> | undefined;
-  // p5_canvas: ElementRef<HTMLCanvasElement> | undefined;
 
-  ngOnInit() {
+  width = 250;
+  height = 80;
 
-  }
-
+  isEngineRun = false;
   ngAfterViewInit() {
 
     const s = (sketch: any) => {
       let engine = Engine.create();
       sketch.setup = () => {
 
-        let magicCloth = sketch.createCanvas(500, 500);
-        magicCloth.parent('p5_div');
-        console.log("magicCloth.id(): ", magicCloth.id());
+        let p5Canvas = sketch.createCanvas(this.width, this.height);
+        p5Canvas.parent('p5_div');
 
+        let p5CanvasIdSelector = '#' + p5Canvas.id();
+        let p5CanvasElement = this.p5_div?.nativeElement.querySelector(p5CanvasIdSelector) as any;
+        p5CanvasElement.className = 'rounded-full ';
 
         let render = Render.create({
-          canvas: this.p5_div?.nativeElement.querySelector('#'+magicCloth.id()) as any,
+          canvas: p5CanvasElement,
           engine: engine,
           options: {
-            width: 500,
-            height: 500,
+            width: this.width,
+            height: this.height,
             wireframes: false,
+            background: 'rgba(0,255,0, 0.25)'
           },
         });
-        let boxA = Bodies.rectangle(400, 200, 80, 80);
-        World.add(engine.world, [boxA]);
-        Runner.run(engine);
         Render.run(render);
-
-        sketch.background(0);
       };
 
       sketch.draw = () => {
-        
-        // sketch.ellipse(sketch.mouseX, sketch.mouseY, 80, 80);
       };
+
       sketch.mouseClicked = () => {
-        let boxA = Bodies.rectangle(sketch.mouseX, sketch.mouseY, 80, 80);
+
+        let boxA = this.getABox(sketch.mouseX, sketch.mouseY);
         World.add(engine.world, [boxA]);
+
+        if (!this.isEngineRun && engine.world.bodies.length > 25) {
+          Runner.run(engine); // 啟動物理引擎
+          this.isEngineRun = true;
+        }
       }
     };
     new p5(s);
-
-    console.log("this.matter_canvas");
-    console.log(this.matter_canvas);
-    // this.demo();
   }
 
-  demo() {
-    let engine = Engine.create();
-    let render = Render.create({
-      canvas: this.matter_canvas?.nativeElement,
-      engine: engine,
-      options: {
-        width: 800,
-        height: 400,
-        wireframes: false,
-      },
+  getABox(x: number, y: number) {
+    return Bodies.rectangle(x, y, 15, 15, {
+      render: {
+        fillStyle: '#000000'
+      }
     });
-    let boxA = Bodies.rectangle(400, 200, 80, 80);
-    let ballA = Bodies.circle(380, 100, 40, {});
-    let ballB = Bodies.circle(460, 10, 40, {});
-    let ground = Bodies.rectangle(400, 380, 810, 60, {
-      isStatic: true,
-    });
-    World.add(engine.world, [boxA, ballA, ballB, ground]);
-    Runner.run(engine);
-    Render.run(render);
-
   }
 
 }
